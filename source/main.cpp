8dcb93670f8343efd4f3dbe2be8a1fe19ace558b
trlla2 <116338442+trlla2@users.noreply.github.com>
2025-10-29 16:20:06 +0100

    render

--- source/main.cpp
+++ source/main.cpp
@@ -1,7 +1,55 @@
 #include <SDL3/SDL.h>
+#include <exception>
+#include <iostream>
 
 int main() {
-	if (!SDL_Init(SDL_INIT_VIDEO))
-		throw SDL_GetError();
+
+	bool _isRunning;
+	SDL_Window* _window = nullptr;
+	SDL_Renderer* _renderer = nullptr;
+
+
+	try 
+	{
+		if (!SDL_Init(SDL_INIT_VIDEO))
+			throw SDL_GetError();
+
+		if (!SDL_CreateWindowAndRenderer(
+			"Test",
+			512, 512,
+			SDL_WINDOW_RESIZABLE,
+			&_window,
+			&_renderer
+		))
+			throw SDL_GetError();
+
+		SDL_SetRenderDrawColor(_renderer, 254,0,0,0xFF);
+
+		_isRunning = true;
+	}
+	catch (std::exception& e)
+	{
+		SDL_DestroyRenderer(_renderer);
+		SDL_DestroyWindow(_window);
+		SDL_Quit();
+		return -1;
+	}
+	
+	while (_isRunning) 
+	{
+		SDL_Event event;
+
+		while (SDL_PollEvent(&event)) 
+			if(event.type == SDL_EVENT_QUIT)
+				_isRunning = false;
+
+		SDL_RenderClear(_renderer);
+		SDL_RenderPresent(_renderer);
+	}
+	
+	SDL_DestroyRenderer(_renderer);
+	SDL_DestroyWindow(_window);
+	SDL_Quit();
+
 	return 0;
 }
